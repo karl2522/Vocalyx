e12a9a038febdd229cfe035d64ee41aa65efc755
A <vanessleonard.capuras@cit.edu>
2025-02-19 19:34:00 +0800

    Integrating Front and Back

--- backend/backend/users/models.py
+++ backend/backend/users/models.py
@@ -4,12 +4,17 @@ from django.db import models
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    full_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     email_verified = models.BooleanField(default=False)
     email_verification_token = models.CharField(max_length=100, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'full_name']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.email
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
--- backend/backend/users/serializers.py
+++ backend/backend/users/serializers.py
@@ -10,7 +10,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('full_name', 'email', 'password', 'password2', 'username')
+        fields = ('first_name', 'last_name', 'email', 'password', 'password2')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -19,23 +19,27 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password2')
+
+        username = validated_data['email'].split('@')[0]
+
         user = CustomUser.objects.create_user(
-            username=validated_data['username'],
+            username=username,
             email=validated_data['email'],
-            full_name=validated_data['full_name'],
-            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
         )
         return user
 
 class UserLoginSerializer(serializers.Serializer):
-    email_or_username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
     def validate(self, attrs):
-        email_or_username = attrs.get('email_or_username')
+        email = attrs.get('email')
         password = attrs.get('password')
 
-        if not email_or_username or not password:
-            raise serializers.ValidationError('Both email/username and password are required.')
+        if not email or not password:
+            raise serializers.ValidationError('Both email and password are required.')
 
         return attrs
