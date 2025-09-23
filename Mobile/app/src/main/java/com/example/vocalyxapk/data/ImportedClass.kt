d9891a37402d4ba07b39bd36054d1a39301dbc8b
A <vanessleonard.capuras@cit.edu>
2025-09-23 18:03:38 +0800

    delete mobile

--- Mobile/app/src/main/java/com/example/vocalyxapk/data/ImportedClass.kt
+++
@@ -1,7 +0,0 @@
-package com.example.vocalyxapk.data
-
-data class ImportedClass(
-    val name: String,
-    val section: String,
-    val fileData: List<List<String>>
-) 
--- Mobile/app/src/main/java/com/example/vocalyxapk/models/CourseCreateRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vocalyxapk.models
-
-data class CourseCreateRequest(
-    val name: String,
-    val courseCode: String,
-    val description: String? = null,
-    val semester: String,
-    val academic_year: String? = null
-)
