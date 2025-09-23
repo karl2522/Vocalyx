1fd7bbdfe28bae379cfb20101289442d8bd38978
A <vanessleonard.capuras@cit.edu>
2025-09-23 17:59:11 +0800

    commit

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -262,6 +262,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
